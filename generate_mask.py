d22a2e586d8511a25f7f3318d09a50b22e0710a6
hank-kuo-cs <hank.cs04@nctu.edu.tw>
2020-05-23 17:16:34 +0800

    generate mask dataset

--- generate_mask.py
+++ generate_mask.py
@@ -1,43 +1,38 @@
 import os
 import cv2
 from tqdm import tqdm
+from glob import glob
 from FurnitureStyleTransfer.renderer import Pytorch3DRenderer
-from FurnitureStyleTransfer.data import TripletFurnitureDataset
-from FurnitureStyleTransfer.object import Furniture
+from FurnitureStyleTransfer.object import Furniture, FurnitureClass
 from FurnitureStyleTransfer.config import config
 
 
-IMAGE_DATASET_PATH = config.dataset.furniture_images_dataset_path
+SHAPE_DATASET_PATH = config.dataset.shape_net_dataset_path
 
 
 def generate_mask_dataset():
-    train_loader = TripletFurnitureDataset('train').data_loader
-    train_triplet_furniture_list = train_loader.triplet_furniture_list
+    class_names = FurnitureClass.class_names
 
-    test_loader = TripletFurnitureDataset('test').data_loader
-    test_triplet_furniture_list = test_loader.triplet_furniture_list
+    for class_name in class_names:
+        print('\nGenerate mask images of "%s" dataset' % class_name)
+        objects_of_one_class_dataset_path = glob(os.path.join(SHAPE_DATASET_PATH, class_name, '*'))
 
-    print('Generate train mask dataset..')
-    for triplet_furniture in tqdm(train_triplet_furniture_list):
-        for i in range(3):
-            render_15_mask_images(triplet_furniture[i])
-
-    print('Generate test mask dataset..')
-    for triplet_furniture in tqdm(test_triplet_furniture_list):
-        for i in range(3):
-            render_15_mask_images(triplet_furniture[i])
+        for object_path in tqdm(objects_of_one_class_dataset_path):
+            render_15_mask_images(class_name, object_path)
 
 
-def render_15_mask_images(furniture: Furniture):
-    assert isinstance(furniture, Furniture)
+def render_15_mask_images(class_name: str, object_path: str):
+    assert isinstance(class_name, str)
+    assert isinstance(object_path, str)
 
-    mask_images_path = get_mask_images_path(furniture)
+    mask_images_path = os.path.join(object_path, 'mask_images')
+    obj_path = os.path.join(object_path, 'model.obj')
 
     if os.path.exists(mask_images_path):
         return
     os.makedirs(mask_images_path, exist_ok=True)
 
-    renderer = Pytorch3DRenderer(furniture.obj_path)
+    renderer = Pytorch3DRenderer(obj_path)
 
     for i in range(15):
         azim = i * (360.0 / 15)
@@ -48,10 +43,6 @@ def render_15_mask_images(furniture: Furniture):
         img_path = '%s/mask-%d.png' % (mask_images_path, i)
 
         cv2.imwrite(img_path, mask_image)
-
-
-def get_mask_images_path(furniture) -> str:
-    return os.path.join(IMAGE_DATASET_PATH, furniture.dataset_type, furniture.furniture_class_str, '3dw', furniture.furniture_id, 'mask_images')
 
 
 if __name__ == '__main__':
