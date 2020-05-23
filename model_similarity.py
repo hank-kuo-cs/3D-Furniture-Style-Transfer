3c759bfeeebcaf4ac8db42e596a8118fa44b9991
hank-kuo-cs <hank.cs04@nctu.edu.tw>
2020-05-23 22:33:17 +0800

    divide different class of similarity dataset file

--- model_similarity.py
+++ model_similarity.py
@@ -1,3 +1,4 @@
+
 import json
 import os
 import cv2
@@ -7,8 +8,9 @@ from glob import glob
 from tqdm import tqdm
 from FurnitureStyleTransfer.object import FurnitureClass
 
-shapenet_dataset_path = '/Users/hank/Desktop/Shape-Style-Transfer/ShapeNet'
+shapenet_dataset_path = '/data/hank/Shape-Style-Transfer/ShapeNet'
 similar_json = {}
+os.makedirs('ModelSimilarity', exist_ok=True)
 
 
 def add_data_to_json(class_type: str, object_id: str, similar_object_id):
@@ -27,10 +29,14 @@ def add_data_to_json(class_type: str, object_id: str, similar_object_id):
     similar_json[class_type][object_id] = similar_object_id
 
 
-def save_json():
+def save_json(class_name: str):
+    global similar_json
+
     data = json.dumps(similar_json)
-    with open('similarity.json', 'w') as f:
+    with open('ModelSimilarity/%s.json' % class_name, 'w') as f:
         f.write(data)
+    f.close()
+    similar_json.clear()
 
 
 def get_mask_l1_loss(mask1, mask2):
@@ -48,9 +54,6 @@ def get_similarity_loss_two_objects(class_type: str, obj1_id: str, obj2_id: str)
     loss = 0.0
 
     for mask1, mask2 in zip(obj1_masks_path, obj2_masks_path):
-        print(mask1)
-        print(mask2)
-        print()
 
         mask1 = cv2.imread(mask1, cv2.IMREAD_GRAYSCALE)
         mask2 = cv2.imread(mask2, cv2.IMREAD_GRAYSCALE)
@@ -74,7 +77,7 @@ if __name__ == '__main__':
         for object1_dir_path in tqdm(objects_of_one_class_dataset_path):
             best_loss = -1
 
-            for object2_dir_path in tqdm(objects_of_one_class_dataset_path):
+            for object2_dir_path in objects_of_one_class_dataset_path:
                 if object1_dir_path == object2_dir_path:
                     continue
 
@@ -87,4 +90,4 @@ if __name__ == '__main__':
                     best_loss = similarity_loss
                     add_data_to_json(class_name, obj1_id, obj2_id)
 
-    save_json()
+        save_json(class_name)
